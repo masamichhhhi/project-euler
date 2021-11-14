0d5cfde1e99a835111dba18ddd4a2399f3fa1ac6
masamichhhhi <masamichhhi@gmail.com>
2021-11-15 03:14:55 +0900

    solve and add answer 009

--- 009/main.go
+++ 009/main.go
@@ -1,12 +1,46 @@
 package main
 
+import "fmt"
+
 // A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
 
-// a2 + b2 = c2
-// For example, 32 + 42 = 9 + 16 = 25 = 52.
+// a^2 + b^2 = c2
+// For example, 3^2 + 4^2 = 9 + 16 = 25 = 52.
 
 // There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 // Find the product abc.
-func main() {
 
+func isPythagorean(a, b, c int) bool {
+	if (a*a + b*b) == c*c {
+		return true
+	} else {
+		return false
+	}
+
+}
+
+func isSum1000(a, b, c int) bool {
+	if a+b+c == 1000 {
+		return true
+	} else {
+		return false
+	}
+}
+
+func main() {
+	var result int
+
+	for a := 1; a < 1000; a++ {
+		for b := 1; b < 1000; b++ {
+			for c := 1; c < 1000; c++ {
+				if isPythagorean(a, b, c) && isSum1000(a, b, c) {
+					result = a * b * c
+					break
+				}
+			}
+		}
+
+	}
+
+	fmt.Println(result)
 }
